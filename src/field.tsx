8d815edade4b90ae1935ffc5f6229e16a7af3d15
inxole <inxole@gmail.com>
2023-10-19 22:34:16 +0900

    Complete camera control

--- src/field.tsx
+++ src/field.tsx
@@ -1,20 +1,31 @@
-import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei"
-import { Suspense } from "react"
+import { ContactShadows, Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei"
+import { Suspense, useEffect } from "react"
 import Bear_Precious_Baked from "./Model/Bear_Pretty_Bake"
+import { Vector3 } from "three"
+import { useThree } from "@react-three/fiber"
 
 export const Field = () => {
+    const { camera } = useThree()
+
+    useEffect(() => {
+        camera.lookAt(new Vector3(0, 1, 0))
+    }, [camera])
     return (
         <Suspense fallback={null}>
-            <OrbitControls />
-            <PerspectiveCamera makeDefault position={[1.0, 0.6, 1.0]} />
-            <group name='light_01' position={[0.2, 0.5, 0.2]} rotation={[-Math.PI / 1, 1, 1]}>
-                <pointLight intensity={1} /></group>
-            <group name='light_02' position={[-0.2, -0.5, -0.2]} rotation={[-Math.PI / 1, 1, 1]}>
-                <pointLight intensity={1} /></group>
+            <OrbitControls target={new Vector3(0, 0.35, 0)} />
+            <PerspectiveCamera makeDefault position={[1, 1, 1]}>
+            </PerspectiveCamera>
 
-            <Bear_Precious_Baked position={[0, -0.25, 0]}></Bear_Precious_Baked>
+            <Bear_Precious_Baked position={[0, 0, 0]}></Bear_Precious_Baked>
 
-            <Environment preset="city" />
+            <ContactShadows
+                opacity={0.5}
+                blur={0.1}
+                far={10}
+                resolution={1024}
+                color="red"
+                position={[0, 0, 0]} />
+            <Environment preset='forest' />
         </Suspense>
     )
 }
