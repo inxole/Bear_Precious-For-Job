400c1c9f357a091e7ff0ac11e51446e26c1ed0e0
inxole <inxole@gmail.com>
2023-11-21 20:37:15 +0900

    fixed wheelevent error  and appdate package

--- src/field.tsx
+++ src/field.tsx
@@ -1,12 +1,14 @@
 import { ContactShadows, Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { Suspense, useEffect } from "react"
-import Bear_Precious_Baked from "./Model/Bear_Pretty_Bake"
 import { Vector3 } from "three"
+// import * as THREE from "three"
 import { useThree } from "@react-three/fiber"
+import Bear_Pretty_Latest from "./Model/Bear_Pretty"
+// import { RippleRenderer } from "./Distortion"
 
 export const Field = () => {
     const { camera } = useThree()
-
+    const viewinfo = window.document
     useEffect(() => {
         camera.lookAt(new Vector3(0, 1, 0))
     }, [camera])
@@ -17,11 +19,12 @@ export const Field = () => {
                 minDistance={0.6}
                 maxDistance={5}
                 target={new Vector3(0, 0.4, 0)}
+                domElement={viewinfo.documentElement}
             />
             <PerspectiveCamera makeDefault position={[1, 1, 1]}>
             </PerspectiveCamera>
 
-            <Bear_Precious_Baked position={[0, 0, 0]}></Bear_Precious_Baked>
+            <Bear_Pretty_Latest position={[0, 0, 0]} ></Bear_Pretty_Latest>
 
             <ContactShadows
                 opacity={0.5}
@@ -35,17 +38,21 @@ export const Field = () => {
     )
 }
 
-// const onw = () => {
-//     console.log(onwheel)
+// const animationRunning = false
+// const texture = new THREE.Texture()
+// const rippleEffect = new RippleRenderer(texture)
+
+// const animate = () => {
+//     rippleEffect.update(renderer, uniform)
+
+//     if (animationRunning) {
+//         requestAnimationFrame(animate);
+//     }
 // }
 
-// window.addEventListener('wheel', function () {
-//     onw()
-//     //????
-// }, { passive: false })
-
-//参考
-//https://www.uriports.com/blog/easy-fix-for-unable-to-preventdefault-inside-passive-event-listener/
-//https://qiita.com/teamhimeH/items/d5c3e3dfcc6cb63456c6
-//https://stackoverflow.com/questions/42101723/unable-to-preventdefault-inside-passive-event-listener
-//https://blog.jxck.io/entries/2016-06-09/passive-event-listeners.html
+// const handleClick = () => {
+//     if (!animationRunning) {
+//         animationRunning = true
+//         animate()
+//     }
+// }
