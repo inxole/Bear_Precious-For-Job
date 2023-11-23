6e9cefcb251eceb245c3c873478a595b8aafed8f
inxole <inxole@gmail.com>
2023-11-23 19:13:21 +0900

    missed add loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Canvas } from "@react-three/fiber"
 import { Field } from "./Field"
+import { Loader } from "@react-three/drei"
 
 function App() {
+  const loaderStyle = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  }
   return (
     <div className="Bear_Precious">
       <Canvas>
         <Field></Field>
       </Canvas>
+      <Loader innerStyles={loaderStyle} />
     </div>
   )
 }
