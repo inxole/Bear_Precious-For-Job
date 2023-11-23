e35ddd46075279624b03a67a6400d6413561f179
inxole <inxole@gmail.com>
2023-11-23 14:13:20 +0900

    unfixed wheel event in Field.tsx

--- src/field.tsx
+++ src/field.tsx
@@ -1,9 +1,9 @@
-import { ContactShadows, Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei"
+import { ContactShadows, Environment, OrbitControls, OrbitControlsChangeEvent, PerspectiveCamera } from "@react-three/drei"
 import { Suspense, useEffect } from "react"
 import { Vector3 } from "three"
-// import * as THREE from "three"
-import { useThree } from "@react-three/fiber"
+import { ThreeEvent, useThree } from "@react-three/fiber"
 import Bear_Pretty_Latest from "./Model/Bear_Pretty"
+// import * as THREE from "three"
 // import { RippleRenderer } from "./Distortion"
 
 export const Field = () => {
@@ -13,13 +13,39 @@ export const Field = () => {
         camera.lookAt(new Vector3(0, 1, 0))
     }, [camera])
 
+    interface ExtendedOrbitControlsProps {
+        id: string;
+        minDistance: number
+        maxDistance: number
+        target: Vector3
+        enablePan: boolean
+        domElement: HTMLElement
+        onChange: ((e: OrbitControlsChangeEvent | undefined) => void) | undefined
+        onWheel: ((event: ThreeEvent<WheelEvent>) => void) | undefined
+    }
+
+    const ExtendedOrbitControls: React.FC<ExtendedOrbitControlsProps> = ({ ...props }) => {
+        return <OrbitControls {...props} />
+    }
+
+    const WheelAction = document.getElementById('test')
+    const Wheelpool = () => {
+        WheelAction?.addEventListener('wheel', function OnMouseWheel(event: WheelEvent) {
+            event.preventDefault()
+        }, { passive: true })
+    }
+
     return (
         <Suspense fallback={null}>
-            <OrbitControls
+            <ExtendedOrbitControls
+                id={"test"}
                 minDistance={0.6}
                 maxDistance={5}
                 target={new Vector3(0, 0.4, 0)}
+                enablePan={false}
                 domElement={viewinfo.documentElement}
+                onChange={() => { Wheelpool }}
+                onWheel={() => { Wheelpool }}
             />
             <PerspectiveCamera makeDefault position={[1, 1, 1]}>
             </PerspectiveCamera>
@@ -56,3 +82,8 @@ export const Field = () => {
 //         animate()
 //     }
 // }
+
+//https://www.uriports.com/blog/easy-fix-for-unable-to-preventdefault-inside-passive-event-listener/
+//https://qiita.com/teamhimeH/items/d5c3e3dfcc6cb63456c6
+//https://stackoverflow.com/questions/42101723/unable-to-preventdefault-inside-passive-event-listener
+//https://blog.jxck.io/entries/2016-06-09/passive-event-listeners.html
