91fdaccc6b06b5b964289bc97187ad6abd5852dc
inxole <inxole@gmail.com>
2023-11-23 22:48:05 +0900

    fixed test movie URL and change vite.config

--- src/Link_Movie.tsx
+++ src/Link_Movie.tsx
@@ -3,37 +3,43 @@ import { useRecoilState } from "recoil"
 import { Text_Hovered } from "./Bear_atom"
 
 const Link_to_Movie = () => {
-
     const [, setIsHovered] = useRecoilState(Text_Hovered)
 
     return (
-        <Html
-            onPointerOver={() => { setIsHovered(true) }} onPointerOut={() => { setIsHovered(false) }}
-            scale={0.01}
-            rotation={[-Math.PI / 2, 0, 0]}
-            position={[1.5, 3, 0]}
-            transform
-            occlude>
-            <div
+        <mesh>
+            <Html
                 onPointerOver={() => { setIsHovered(true) }} onPointerOut={() => { setIsHovered(false) }}
-                style={{
-                    cursor: 'pointer',
-                    width: '2000px',
-                    height: '600px',
-                    fontSize: '300px',
-                    fontWeight: 300,
-                    background: 'black',
-                    color: '#f0f0f0',
-                    padding: '2px 10px',
-                    borderRadius: '200px',
-                    letterSpacing: '1px',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    gap: '5px'
-                }}
-            >動画ULR</div>
-        </Html>
+                scale={0.1}
+                rotation={[-Math.PI / 2, 0, 0]}
+                position={[0.5, 1, -4.5]}
+                transform
+            >
+                <div
+                    onWheel={(e) => e.stopPropagation()}
+                    onPointerOver={() => { setIsHovered(true) }} onPointerOut={() => { setIsHovered(false) }}
+                    style={{
+                        cursor: 'pointer',
+                        width: '600px',
+                        height: '150px',
+                        fontSize: '100px',
+                        fontWeight: 100,
+                        background: 'black',
+                        color: '#f0f0f0',
+                        padding: '2px 10px',
+                        borderRadius: '20px',
+                        letterSpacing: '1px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        gap: '5px'
+                    }}
+                >
+                    <a 
+                    // href="https://www.google.com" target="_blank" rel="noopener noreferrer"
+                    >動画へ</a>
+                </div>
+            </Html>
+        </mesh>
     )
 }
 
