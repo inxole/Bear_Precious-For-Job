5951afdf9b4d2a18f68bfe18fea50189f3f52d77
inxole <inxole@gmail.com>
2023-11-24 19:19:55 +0900

    fixed react-three-drei HTML bug

--- src/Link_Movie.tsx
+++ src/Link_Movie.tsx
@@ -1,41 +1,38 @@
 import { Html } from "@react-three/drei"
-import { useRecoilState } from "recoil"
-import { Text_Hovered } from "./Bear_atom"
 
 const Link_to_Movie = () => {
-    const [, setIsHovered] = useRecoilState(Text_Hovered)
 
     return (
         <mesh>
             <Html
-                onPointerOver={() => { setIsHovered(true) }} onPointerOut={() => { setIsHovered(false) }}
-                scale={0.1}
+                scale={0.01}
                 rotation={[-Math.PI / 2, 0, 0]}
-                position={[0.5, 1, -4.5]}
+                position={[0, 0, 0]}
                 transform
+                pointerEvents={'none'}
             >
                 <div
                     onWheel={(e) => e.stopPropagation()}
-                    onPointerOver={() => { setIsHovered(true) }} onPointerOut={() => { setIsHovered(false) }}
                     style={{
+                        pointerEvents: 'auto',
                         cursor: 'pointer',
-                        width: '600px',
-                        height: '150px',
-                        fontSize: '100px',
+                        width: '6000px',
+                        height: '1500px',
+                        fontSize: '1000px',
                         fontWeight: 100,
                         background: 'black',
                         color: '#f0f0f0',
-                        padding: '2px 10px',
-                        borderRadius: '20px',
-                        letterSpacing: '1px',
+                        padding: '20px 100px',
+                        borderRadius: '200px',
+                        letterSpacing: '10px',
                         display: 'flex',
                         justifyContent: 'center',
                         alignItems: 'center',
-                        gap: '5px'
+                        gap: '50px'
                     }}
                 >
-                    <a 
-                    // href="https://www.google.com" target="_blank" rel="noopener noreferrer"
+                    <a
+                        href="https://www.google.com" target="_blank" rel="noopener noreferrer"
                     >動画へ</a>
                 </div>
             </Html>
