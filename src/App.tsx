a10c9c97303e9fd04c24f7f5e46379a1541d5568
inxole <inxole@gmail.com>
2023-12-16 03:21:21 +0900

    add operable effects and delete file

--- src/App.tsx
+++ src/App.tsx
@@ -2,101 +2,49 @@ import { Canvas } from "@react-three/fiber"
 import { Field } from "./Field"
 import { Loader } from "@react-three/drei"
 import Zoom_ON_OFF from "./Zoom_Button"
-import { useCallback, useEffect, useRef, useState } from "react"
-import { Bear_Hovered } from "./Bear_atom"
-import { useRecoilState } from "recoil"
+import { useEffect, useRef } from "react"
+import '../rippleEffect.css'
+
+class Ripple {
+  element: HTMLDivElement
+  animationDuration: number = 600 // アニメーションの持続時間
+
+  constructor(x: number, y: number) {
+    this.element = document.createElement('div')
+    this.element.classList.add('ripple')
+    this.element.style.left = `${x}px`
+    this.element.style.top = `${y}px`
+
+    this.element.addEventListener('animationend', () => {
+      this.element.remove()
+    })
+  }
+}
 
 function App() {
-  const loaderStyle = { position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }
-
-  const [ripple_trigger,] = useRecoilState(Bear_Hovered)
-  const [count, setCount] = useState(0)
   const canvasRef = useRef<HTMLDivElement>(null)
 
-  const getRandomColor = () => {
-    const minColorValue = 100
-    const red = Math.floor(Math.random() * (256 - minColorValue) + minColorValue)
-    const green = Math.floor(Math.random() * (256 - minColorValue) + minColorValue)
-    const blue = Math.floor(Math.random() * (256 - minColorValue) + minColorValue)
-    const alpha = 0.3
-    return `rgba(${red}, ${green}, ${blue}, ${alpha})`
-  }
+  useEffect(() => {
+    const container = canvasRef.current
+    if (!container) return
 
-  const createRippleEffect = useCallback((): HTMLElement => {
-    const ripple = document.createElement("span")
-    ripple.style.position = 'fixed'
-    ripple.style.borderRadius = '50%'
-    ripple.style.transform = 'scale(0)'
-    ripple.style.animation = 'ripple-effect 600ms linear'
-    ripple.style.backgroundColor = getRandomColor()
+    const canvasClick = (e: MouseEvent) => {
+      const x = e.clientX
+      const y = e.clientY
+      const ripple = new Ripple(x, y)
+      container.appendChild(ripple.element)
+    };
 
-    return ripple
+    container.addEventListener('click', canvasClick)
+
+    return () => {
+      container.removeEventListener('click', canvasClick)
+    }
   }, [])
 
-  const handleCanvasClick = useCallback((event: MouseEvent) => {
-    if (ripple_trigger) {
-      setCount(count + 1)
-      const canvas = canvasRef.current
-      if (!canvas) return
-      const rect = canvas.getBoundingClientRect()
-      const x = event.clientX - rect.left
-      const y = event.clientY - rect.top
-
-      // 複数の波紋を生成
-      for (let i = 0; i < 8; i++) {
-        const ripple = createRippleEffect()
-        ripple.style.width = ripple.style.height = `${100 + i * 20}px`
-        ripple.style.left = `${x - (50 + i * 10)}px`
-        ripple.style.top = `${y - (50 + i * 10)}px`
-
-        canvas.appendChild(ripple)
-
-        setTimeout(() => {
-          ripple.remove()
-        }, 600 + i * 100)
-      }
-    }
-  }, [createRippleEffect, ripple_trigger, count])
-
-  const handleCanvasClickRef = useRef(handleCanvasClick)
-
-  useEffect(() => {
-    handleCanvasClickRef.current = handleCanvasClick
-  }, [handleCanvasClick])
-
-  useEffect(() => {
-    const styleSheet = document.createElement("style")
-    if (count % 2 == 0) {
-      // スタイルシートの追加
-      styleSheet.textContent = `
-      @keyframes ripple-effect {
-        from {opacity: 0;transform: scale(8);}
-        to {opacity: Math.random() * 0.8;transform: scale(0);}
-      }`
-    } else {
-      // スタイルシートの追加
-      styleSheet.textContent = `
-        @keyframes ripple-effect {
-          from {opacity: Math.random() * 0.8;transform: scale(0);}
-          to {opacity: 0;transform: scale(8);}
-        }`
-    }
-    document.head.appendChild(styleSheet)
-
-    // スタイルシートとイベントリスナーの設定
-    const canvas = canvasRef.current
-    if (canvas) {
-      const eventHandler = (event: MouseEvent) => handleCanvasClickRef.current(event)
-      canvas.addEventListener('click', eventHandler)
-      return () => {
-        canvas.removeEventListener('click', eventHandler)
-        document.head.removeChild(styleSheet)
-      }
-    }
-  }, [ripple_trigger, count])
-
+  const loaderStyle = { position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }
   return (
-    <div className="Bear_Precious" ref={canvasRef} style={{ position: 'relative' }}>
+    <div className="Bear_Precious" ref={canvasRef} style={{ position: 'relative' }} >
       <Zoom_ON_OFF />
       <Canvas>
         <Field></Field>
