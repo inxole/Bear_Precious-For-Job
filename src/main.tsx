9674aa544cfa005bbc2abad2795acb46ba7c6941
inxole <inxole@gmail.com>
2025-01-07 23:52:04 +0900

    chore: remove Recoil and related dependencies, update to Jotai for state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RecoilRoot } from 'recoil'
 import Check_webGL from './webGL_check.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <Check_webGL />
-    </RecoilRoot>
+    <Check_webGL />
   </React.StrictMode>
 )
